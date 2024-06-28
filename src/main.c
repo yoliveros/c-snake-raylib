abbb200fc22515208af4c0486154b3112d6ef988
yoliveros <28690224+yoliveros@users.noreply.github.com>
2024-06-27 20:07:10 -0400

    feat: nose

--- src/main.c
+++ src/main.c
@@ -8,10 +8,23 @@ int main() {
 
   SetTargetFPS(144);
 
+  Vector2 square_position = {(float)screenWidth / 2, (float)screenHeight / 2};
+
   while (!WindowShouldClose()) {
+    // update
+    if (IsKeyDown(KEY_RIGHT))
+      ballPosition.x += 2.0f;
+    if (IsKeyDown(KEY_LEFT))
+      ballPosition.x -= 2.0f;
+    if (IsKeyDown(KEY_UP))
+      ballPosition.y -= 2.0f;
+    if (IsKeyDown(KEY_DOWN))
+      ballPosition.y += 2.0f;
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawText("GG Enorawena", 190, 200, 20, LIGHTGRAY);
+    DrawRectangle(square_position, 25, 25, MAROON);
     EndDrawing();
   }
 
