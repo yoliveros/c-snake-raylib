5121e2eec6f97d21e0bd473ac906ecbc56be4f35
yoliveros <28690224+yoliveros@users.noreply.github.com>
2024-07-01 09:16:22 -0400

    feat: build script changed

--- build.sh
+++ build.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-dir="out"
+# Project name
+p_name='snake'
 
-if [ ! -d $dir ]; then
-  meson setup $dir
+# Setup and build folders
+out="out"
+bin="bin"
+
+# Release folders
+out_dist="meson-dist"
+dist="dist"
+
+# 
+meson setup $out
+
+if [ ! -z $1 ] && [[ $1 == "prod" ]];then
+  meson dist -C $out --allow-dirty
+
+  mv $out/$out_dist $dist 
+else
+  if [ ! -d $bin ]; then
+    mkdir $bin
+  fi
+  
+  meson compile -C $out
+  
+  mv $out/$p_name $bin
 fi
 
-meson compile -C $dir
