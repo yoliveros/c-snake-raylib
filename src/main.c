5e116b1d806fd1287efe7fc8d2a265505ba2e275
yoliveros <28690224+yoliveros@users.noreply.github.com>
2024-07-09 19:01:22 -0400

    feat: copy

--- src/main.c
+++ src/main.c
@@ -1,115 +1,115 @@
 #include "include/raylib.h"
-
-#include "SnakeBody.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-#define offset 75
-#define cell_size 30
-#define cell_count 30
-#define interval 0.08
+#define GRID_SIZE 16
+#define SCREEN_WIDTH 512
+#define SCREEN_HEIGHT 512
 
-Vector2 init_head = {(float)cell_count / 2, (float)cell_count / 2};
-
-Vector2 food;
-
-State state = {
-    .direction = {.5, 0},
-    .body = {{0}},
-    .score = 0,
+struct snake_struct {
+  Vector2 position;
+  Vector2 velocity;
 };
 
-void gen_food(Vector2 *value) {
-  int x = rand() % cell_count;
-  int y = rand() % cell_count;
-  *value = (Vector2){x, y};
-}
+int main(void) {
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "snake");
+  bool started = false;
 
-void reset() {
-  restart();
+  struct snake_struct snake[100] = {0};
+  int snake_length = 1;
 
-  state = (State){
-      .direction = {.5, 0},
-      .body = {{0}},
-      .score = 0,
-  };
-
-  en_queue(init_head, false);
-  gen_food(&food);
-}
-
-void snake_movement() {
-  if (IsKeyDown(KEY_RIGHT) && state.direction.x != -.5) {
-    state.direction = (Vector2){.5, 0};
-  }
-  if (IsKeyDown(KEY_LEFT) && state.direction.x != .5) {
-    state.direction = (Vector2){-.5, 0};
-  }
-  if (IsKeyDown(KEY_UP) && state.direction.y != .5) {
-    state.direction = (Vector2){0, -.5};
-  }
-  if (IsKeyDown(KEY_DOWN) && state.direction.y != -.5) {
-    state.direction = (Vector2){0, .5};
+  for (int i = 0; i < snake_length; i++) {
+    snake[i].position.x = 8 + i;
+    snake[i].position.y = 8;
   }
 
-  Vector2 *curr_head = get_rear();
-  Vector2 new_head = {
-      curr_head->x + state.direction.x,
-      curr_head->y + state.direction.y,
-  };
+  Vector2 collect = {11, 3};
 
-  if (new_head.x >= cell_count || new_head.x <= -.5 ||
-      new_head.y >= cell_count || new_head.y <= -.5) {
-    printf("Game over\n");
-    reset();
-    return;
-  }
-
-  en_queue(new_head, true);
-}
-
-int main() {
-  InitWindow(2 * offset + cell_size * cell_count,
-             2 * offset + cell_size * cell_count, "Snake");
-
-  SetTargetFPS(60);
-
-  en_queue(init_head, false);
-
-  gen_food(&food);
-
-  double last_update = 0;
+  int base_fps = 10;
+  SetTargetFPS(base_fps);
 
   while (!WindowShouldClose()) {
-    // update
-    double curr_time = GetTime();
+    int fps = GetFPS();
 
-    if (curr_time - last_update < interval) {
-      continue;
+    if (IsKeyDown(KEY_SPACE)) {
+      if (!started)
+        started = true;
     }
 
-    snake_movement();
+    // SNAKE MOVEMENT
+    if (started) {
+      for (int i = snake_length - 1; i > 0; i--) {
+        snake[i] = snake[i - 1];
+      }
 
-    last_update = curr_time;
+      if (IsKeyDown(KEY_RIGHT)) {
+        snake[0].velocity = (Vector2){1, 0};
+      } else if (IsKeyDown(KEY_LEFT)) {
+        snake[0].velocity = (Vector2){-1, 0};
+      } else if (IsKeyDown(KEY_UP)) {
+        snake[0].velocity = (Vector2){0, -1};
+      } else if (IsKeyDown(KEY_DOWN)) {
+        snake[0].velocity = (Vector2){0, 1};
+      }
 
-    Vector2 *curr_head = get_rear();
+      snake[0].position.x += snake[0].velocity.x;
+      snake[0].position.y += snake[0].velocity.y;
+    }
 
-    printf("%f, %f\n", curr_head->x, curr_head->y);
+    // COLLISIONS AND SCREEN BORDER
+    if (snake[0].position.x >= GRID_SIZE)
+      snake[0].position.x = 0;
+    else if (snake[0].position.x < 0)
+      snake[0].position.x = GRID_SIZE - 1;
 
+    if (snake[0].position.y >= GRID_SIZE)
+      snake[0].position.y = 0;
+    else if (snake[0].position.y < 0)
+      snake[0].position.y = GRID_SIZE - 1;
+
+    if (snake[0].position.x == collect.x && snake[0].position.y == collect.y) {
+      snake_length++;
+      collect.x = GetRandomValue(0, GRID_SIZE - 1);
+      collect.y = GetRandomValue(0, GRID_SIZE - 1);
+    }
+
+    for (int i = 1; i < snake_length; i++) {
+      if (snake[0].position.x == snake[i].position.x &&
+              snake[0].position.y == snake[i].position.y ||
+          IsKeyDown(KEY_R)) {
+        snake_length = 1;
+        snake[0].position.x = 8;
+        snake[0].position.y = 8;
+        started = false;
+      }
+    }
+
+    // DRAWING
     BeginDrawing();
     ClearBackground(GREEN);
-    DrawRectangle(offset + food.x * cell_size - 1, offset + food.y * cell_size,
-                  20, 20, RED);
-    DrawRectangle(offset + curr_head->x * cell_size - 1,
-                  offset + curr_head->y * cell_size, 20, 20, WHITE);
-    DrawText("Snake", offset - 5, 20, 40, DARKGREEN);
-    Rectangle rect = {(float)offset - 5, (float)offset - 5,
-                      (float)cell_size * cell_count + 10,
-                      (float)cell_size * cell_count + 10};
-    DrawRectangleLinesEx(rect, 5, DARKGREEN);
-    DrawText(TextFormat("%i", state.score), offset - 5,
-             offset + cell_size * cell_count + 10, 40, DARKGREEN);
+
+    for (int i = 0; i < snake_length; ++i) {
+      Rectangle rect = {snake[i].position.x * SCREEN_WIDTH / GRID_SIZE,
+                        snake[i].position.y * SCREEN_HEIGHT / GRID_SIZE,
+                        SCREEN_WIDTH / GRID_SIZE, SCREEN_HEIGHT / GRID_SIZE};
+
+      DrawRectangleRec(rect, WHITE);
+    }
+
+    DrawRectangle(collect.x * SCREEN_WIDTH / GRID_SIZE,
+                  collect.y * SCREEN_HEIGHT / GRID_SIZE,
+                  SCREEN_WIDTH / GRID_SIZE, SCREEN_HEIGHT / GRID_SIZE, RED);
+
+    // START MESSAGE
+    if (!started) {
+      DrawText("PRESS SPACE TO PLAY", 16, 400, 36, PURPLE);
+    }
+
+    // SCORE
+    char conv[10];
+    snprintf(conv, sizeof(conv), "%d", snake_length);
+
+    DrawText(conv, 256, 16, 36, BLUE);
+
     EndDrawing();
   }
 
